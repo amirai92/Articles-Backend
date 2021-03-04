95197cba69570a9d2b6fec1823ec92db08738de5
amirai92 <34741493+amirai92@users.noreply.github.com>
2021-03-04 22:40:42 +0200

    Finished backend
    Finished the backend added jwt for checking authorization

---
+++ api/models/article.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const articleSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  title: { type: String, required: true, default: "Default Title" },
+  description: { type: String, required: true },
+  content: { type: String, required: true },
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Category",
+  },
+  image: { type: String },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
